5bbdf5b24eea013da4296faf1eaf56f7c165127e
Adam Mathieson <adam@mathieson.dev>
2024-07-15 20:29:29 +0200

    + Small refactor

--- src/App.vue
+++ src/App.vue
@@ -7,29 +7,31 @@ import Device_Config from "./components/Device_Config.vue";
 </script>
 
 <template>
-  <Navbar @back="config_active = false" :back="config_active" :active_page="active_page" @navigate="args => active_page = args"/>
+  <Navbar :active_page="active_page" :back="config_active" @back="config_active = false"
+          @navigate="args => active_page = args"/>
   <transition mode="out-in">
-  <Monitor_Page v-if="active_page === 'Monitor_Page'" @config="config"/>
+    <Monitor_Page v-if="active_page === 'Monitor_Page'" @config="config"/>
   </transition>
   <transition mode="out-in">
-  <System_Page ref="coord" v-if="active_page === 'System_Page'" @config="config"/>
+    <System_Page v-if="active_page === 'System_Page'" ref="coord" @config="config"/>
   </transition>
   <transition mode="out-in">
-  <Coordination_Page v-if="active_page === 'Coordination_Page'"/>
+    <Coordination_Page v-if="active_page === 'Coordination_Page'"/>
   </transition>
   <transition mode="out-in" name="slide-right">
-  <Device_Config @close="config_active = false; device_uid='';device_type=null" :active="config_active" :device_type="device_type" :id="device_uid"/>
+    <Device_Config :id="device_uid" :active="config_active"
+                   :device_type="device_type" @close="config_active = false; device_uid='';device_type=null"/>
   </transition>
 </template>
 <script>
 export default {
-  data: ()=>{
+  data: () => {
     return {
       _endpoint: "https://localhost:7221",
       config_active: false,
-      active_page:"Monitor_Page",
-      transmitterIndexes:{},
-      transmitters:[],
+      active_page: "Monitor_Page",
+      transmitterIndexes: {},
+      transmitters: [],
       socket: null,
       receivers: [],
       receiverIndexes: {},
@@ -40,12 +42,15 @@ export default {
   mounted() {
     this.fetchMics();
     this.fetchReceivers();
-    setInterval(()=>{this.fetchMics();}, 15000)
+    setInterval(() => {
+      this.fetchMics();
+    }, 15000)
     // Create WebSocket connection.
     this.$data.socket = new WebSocket("wss://localhost:7221/ws");
 
 // Connection opened
-    this.$data.socket.addEventListener("open", (event) => {    });
+    this.$data.socket.addEventListener("open", (event) => {
+    });
 
 // Listen for messages
     this.$data.socket.addEventListener("message", (event) => {
@@ -57,12 +62,11 @@ export default {
         this.$data.transmitters[this.$data.transmitterIndexes[data.uid]][data.propertyName] = data.value;
         if (this.$refs.coord)
           this.$refs.coord.updateNetwork();
-      }
-      else{
+      } else {
         data.forEach(element => {
           if (this.$data.transmitterIndexes[element.uid])
             this.$data.transmitters[this.$data.transmitterIndexes[element.uid]].lastMeterData
-            = element.meteringData;
+                = element.meteringData;
           else {
             console.log("Unknown Transmitter: ", element.uid);
           }
@@ -94,7 +98,7 @@ export default {
         response.json().then((data) => {
           this.$data.receivers = data;
           for (const index in data) {
-              this.$data.receiverIndexes[data[index].uid] = index;
+            this.$data.receiverIndexes[data[index].uid] = index;
           }
           if (this.$refs.coord)
             this.$refs.coord.updateNetwork();
